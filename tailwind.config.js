4060af8ea1d204ebf6c6432762509660c7afffc5
leog <leog91@gmail.com>
2025-12-03 22:48:33 +0000

    organization page with kitchen layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ export default {
       fontFamily: {
         // sans: ["Geist", "sans-serif"],
       },
+      writingMode: {
+        "vertical-rl": "vertical-rl",
+        "vertical-lr": "vertical-lr",
+      },
     },
   },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".writing-vertical-rl": {
+          "writing-mode": "vertical-rl",
+        },
+        ".writing-vertical-lr": {
+          "writing-mode": "vertical-lr",
+        },
+      });
+    },
+  ],
 };
